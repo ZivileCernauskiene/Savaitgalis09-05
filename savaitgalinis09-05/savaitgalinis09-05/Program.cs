2c0967d3a8b1f66fd3a4fd5dceab79e8748b208d
ZivileCernauskiene <labadaba@gmail.com>
2021-09-06 18:38:17 +0300

    Naujas VS projektas
    sukurtas .sls
    prideta gitignore failas

---
+++ savaitgalinis09-05/savaitgalinis09-05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace savaitgalinis09_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
